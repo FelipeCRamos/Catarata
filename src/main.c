a5ebeba26a98336fe9b3ac8fd5195d42926c4576
Judson Costa <judson.scosta@hotmail.com>
2018-05-10 17:59:28 -0300

    [bug] arquivo de saída não é criado

--- src/main.c
+++ src/main.c
@@ -1,165 +1,182 @@
-#include "util.h" // header with things that the entire program needs
-#include "read.h" // header with only read things
+#include "util.h"    // header with things that the entire program needs
+#include "read.h"    // header with only read things
 #include "process.h" // header with only process things
-#include "write.h" // header with only write things
+#include "write.h"   // header with only write things
 
 // Defines the DEBUG mode on/off (0/1)
 #define DEBUG 1
 
 int main(int argc, char *argv[])
 {
-	char *filepath = (char *) calloc(256, sizeof(char));
-	char *format = (char *) calloc(8, sizeof(char));
-	char *diagFile = (char *) calloc(256, sizeof(char));
-	// test if all the arguments were provided
-	checkArgs(argc, argv, filepath, format, diagFile);
+  char *filepath = (char *)calloc(256, sizeof(char));
+  char *format = (char *)calloc(8, sizeof(char));
+  char *diagFile = (char *)calloc(256, sizeof(char));
+  // test if all the arguments were provided
+  checkArgs(argc, argv, filepath, format, diagFile);
 
-	// // little welcome interface I did while bored
-	// puts("     ---------------------------------------------------------------------");
-	// puts("    |  _     _  _______  ___      _______  _______  __   __  _______  __  |");
-	// puts("    | | | _ | ||       ||   |    |       ||       ||  |_|  ||       ||  | |");
-	// puts("    | | || || ||    ___||   |    |       ||   _   ||       ||    ___||  | |");
-	// puts("    | |       ||   |___ |   |    |       ||  | |  ||       ||   |___ |  | |");
-	// puts("    | |       ||    ___||   |___ |      _||  |_|  ||       ||    ___||__| |");
-	// puts("    | |   _   ||   |___ |       ||     |_ |       || ||_|| ||   |___  __  |");
-	// puts("    | |__| |__||_______||_______||_______||_______||_|   |_||_______||__| |");
-	// puts("    |                                                                     |");
-	// puts("     ---------------------------------------------------------------------");
-	// puts("___                                                               _             ");
-	// puts(" | _   |_|_  _   _ _ |_ _  _ _  _|_   _|. _  _  _  _  _. _   _ _ (_|_    _  _ _ ");
-	// puts(" |(_)  |_| )(-  (_(_||_(_|| (_|(_|_  (_||(_|(_)| )(_)_)|_)  _)(_)| |_\\)/(_|| (- ");
-	// puts("                                            _/                                  ");
-	// puts("\n\n /\\   _|_|_  _  _ _.");
-	// puts("/~~\\|_|| | |(_)| _\\.");
-	// puts("\n - Felipe C. Ramos Filho\n - João Pedro de A. Paula\n\n\n");
+  // // little welcome interface I did while bored
+  // puts("     ---------------------------------------------------------------------");
+  // puts("    |  _     _  _______  ___      _______  _______  __   __  _______  __  |");
+  // puts("    | | | _ | ||       ||   |    |       ||       ||  |_|  ||       ||  | |");
+  // puts("    | | || || ||    ___||   |    |       ||   _   ||       ||    ___||  | |");
+  // puts("    | |       ||   |___ |   |    |       ||  | |  ||       ||   |___ |  | |");
+  // puts("    | |       ||    ___||   |___ |      _||  |_|  ||       ||    ___||__| |");
+  // puts("    | |   _   ||   |___ |       ||     |_ |       || ||_|| ||   |___  __  |");
+  // puts("    | |__| |__||_______||_______||_______||_______||_|   |_||_______||__| |");
+  // puts("    |                                                                     |");
+  // puts("     ---------------------------------------------------------------------");
+  // puts("___                                                               _             ");
+  // puts(" | _   |_|_  _   _ _ |_ _  _ _  _|_   _|. _  _  _  _  _. _   _ _ (_|_    _  _ _ ");
+  // puts(" |(_)  |_| )(-  (_(_||_(_|| (_|(_|_  (_||(_|(_)| )(_)_)|_)  _)(_)| |_\\)/(_|| (- ");
+  // puts("                                            _/                                  ");
+  // puts("\n\n /\\   _|_|_  _  _ _.");
+  // puts("/~~\\|_|| | |(_)| _\\.");
+  // puts("\n - Felipe C. Ramos Filho\n - João Pedro de A. Paula\n\n\n");
 
-	// these are just auxiliary variables
-	char *filename = stripFilepath(filepath);
-	char *outDir = "test/";
+  // these are just auxiliary variables
+  char *filename = stripFilepath(filepath);
+  char *outDir = "test/";
 
-	// these are some testing printf's, maybe they'll get changed
-	printf("The image to be analised is '%s'.\n", filename);
-	printf("%s's format is '%s'\n", filename, format);
-	printf("The diagnosis is in '%s'.\n", diagFile);
+  // these are some testing printf's, maybe they'll get changed
+  printf("The image to be analised is '%s'.\n", filename);
+  printf("%s's format is '%s'\n", filename, format);
+  printf("The diagnosis is in '%s'.\n", diagFile);
 
-	putchar('\n');
+  putchar('\n');
 
-	Img *originalImg = readPPM(filepath);
-	// Img tempOriginal = *originalImg;
-	// TODO different reading and writing functions for different formats
-	
-	if (!originalImg) {
-		perror(filepath);
-		exit(1);
-	}
+  Img *originalImg = readPPM(filepath);
+  // Img tempOriginal = *originalImg;
+  // TODO different reading and writing functions for different formats
 
-	// here we enter on process.c
-	puts("\n\e[1mStarted processing the image...\e[0m\n");
+  if (!originalImg)
+  {
+    perror(filepath);
+    exit(1);
+  }
 
-	// tone the image to its greyscale
-	Img *greyscaled = greyscale(originalImg);
+  // here we enter on process.c
+  puts("\n\e[1mStarted processing the image...\e[0m\n");
 
-	char *outGrey = outFilepath(outDir, filename, "_grey", format);
-	char *strippedGrey = stripFilepath(outGrey);
-	if (!greyscaled) {
-		fprintf(stderr, "Error writing the greyscaled image to '%s'.\n", strippedGrey);
-		freeImg(originalImg);
-	} else {
-		writePPM(greyscaled, outGrey);
-	}
-	free(outGrey);
-	free(strippedGrey);
+  // tone the image to its greyscale
+  Img *greyscaled = greyscale(originalImg);
 
-	// blur the image with the gaussian filter
-	Img *gaussImg = gaussianFilter(greyscaled, 10);
+  char *outGrey = outFilepath(outDir, filename, "_grey", format);
+  char *strippedGrey = stripFilepath(outGrey);
+  if (!greyscaled)
+  {
+    fprintf(stderr, "Error writing the greyscaled image to '%s'.\n", strippedGrey);
+    freeImg(originalImg);
+  }
+  else
+  {
+    writePPM(greyscaled, outGrey);
+  }
+  free(outGrey);
+  free(strippedGrey);
 
-	char *outGauss = outFilepath(outDir, filename, "_gauss", format);
-	char *strippedGauss = stripFilepath(outGauss);
-	if (!gaussImg) {
-		fprintf(stderr, "Error writing the blurred image to '%s'.\n", strippedGauss);
-	} else {
-		writePPM(gaussImg, outGauss);
-	}
-	free(outGauss);
-	free(strippedGauss);
-	
-	// apply Sobel's filter to enhance the edges
-	Img *sobelImg = sobelFilter(gaussImg, 1);
+  // blur the image with the gaussian filter
+  Img *gaussImg = gaussianFilter(greyscaled, 10);
 
-	char *outSobel = outFilepath(outDir, filename, "_sobel", format);
-	char *strippedSobel = stripFilepath(outSobel);
-	if (!sobelImg) {
-		fprintf(stderr, "Error writing the edge detection image to '%s'.\n", strippedSobel);
-	} else {
-		writePPM(sobelImg, outSobel);
-	}
-	free(outSobel);
-	free(strippedSobel);
+  char *outGauss = outFilepath(outDir, filename, "_gauss", format);
+  char *strippedGauss = stripFilepath(outGauss);
+  if (!gaussImg)
+  {
+    fprintf(stderr, "Error writing the blurred image to '%s'.\n", strippedGauss);
+  }
+  else
+  {
+    writePPM(gaussImg, outGauss);
+  }
+  free(outGauss);
+  free(strippedGauss);
 
-	/** 
+  // apply Sobel's filter to enhance the edges
+  Img *sobelImg = sobelFilter(gaussImg, 1);
+
+  char *outSobel = outFilepath(outDir, filename, "_sobel", format);
+  char *strippedSobel = stripFilepath(outSobel);
+  if (!sobelImg)
+  {
+    fprintf(stderr, "Error writing the edge detection image to '%s'.\n", strippedSobel);
+  }
+  else
+  {
+    writePPM(sobelImg, outSobel);
+  }
+  free(outSobel);
+  free(strippedSobel);
+
+  /** 
 	 * Apply threshold to sobelImg with intensity calculated through Otsu's Method
 	 * pixels bigger than the threshold will be converted to 255 and smaller than
 	 * the threshold will be converted to 0.
 	 */
-	// Img *thresholdImg = otsuMethod(sobelImg);
+  // Img *thresholdImg = otsuMethod(sobelImg);
 
+  int intensity = 0.40 * 255; // default intensy
 
-	int intensity = 0.40 * 255; // default intensy
+  // OPTIMAL SETTINGS
+  if (strcmp(filename, "Catarata.ppm") == 0)
+    intensity = 0.40 * 255;
+  if (strcmp(filename, "Catarata2.ppm") == 0)
+    intensity = 0.40 * 255;
+  if (strcmp(filename, "Normal.ppm") == 0)
+    intensity = 0.23 * 255;
+  if (strcmp(filename, "Normal2.ppm") == 0)
+    intensity = 0.50 * 255;
+  if (strcmp(filename, "F1.ppm") == 0)
+    intensity = 0.50 * 255;
+  if (strcmp(filename, "F2.ppm") == 0)
+    intensity = 0.23 * 255;
 
-	// OPTIMAL SETTINGS
-	if (strcmp(filename, "Catarata.ppm") == 0) intensity = 0.40 * 255;
-	if (strcmp(filename, "Catarata2.ppm") == 0) intensity = 0.40 * 255;
-	if (strcmp(filename, "Normal.ppm") == 0) intensity = 0.23 * 255;
-	if (strcmp(filename, "Normal2.ppm") == 0) intensity = 0.50 * 255;
-	if (strcmp(filename, "F1.ppm") == 0) intensity = 0.50 * 255;
-	if (strcmp(filename, "F2.ppm") == 0) intensity = 0.23 * 255;
+  Img *thresholdImg = threshold(sobelImg, intensity);
 
-	
-	Img *thresholdImg = threshold(sobelImg, intensity);
+  putchar('\n');
 
-	putchar('\n');
+  char *outThreshold = outFilepath(outDir, filename, "_threshold", "pbm");
+  char *strippedThreshold = stripFilepath(outThreshold);
+  if (!thresholdImg)
+  {
+    fprintf(stderr, "Error writing the threshold increase image to '%s'.\n", strippedThreshold);
+  }
+  else
+  {
+    writePPM_PBM(thresholdImg, outThreshold);
+  }
+  free(outThreshold);
+  free(strippedThreshold);
 
-	char *outThreshold = outFilepath(outDir, filename, "_threshold", "pbm");
-	char *strippedThreshold = stripFilepath(outThreshold);
-	if (!thresholdImg){
-		fprintf(stderr, "Error writing the threshold increase image to '%s'.\n", strippedThreshold);
-	} else {
-		writePPM_PBM(thresholdImg, outThreshold);
-	}
-	free(outThreshold);
-	free(strippedThreshold);
-	
-	Img *tempOriginal = readPPM(filepath);
-	Circles *circles = houghMethod(convertImg(thresholdImg));
-	double area = circles->iris[0].area;
+  Img *tempOriginal = readPPM(filepath);
+  Circles *circles = houghMethod(convertImg(thresholdImg));
+  double area = circles->iris[0].area;
 
-	drawIris(tempOriginal, outFilepath(outDir, filename, "_iris", "pbm"), circles->iris);
-	segIris(tempOriginal, outFilepath(outDir, filename, "_seg_iris", "pbm"), circles->iris);
+  drawIris(tempOriginal, outFilepath(outDir, filename, "_iris", "pbm"), circles->iris);
+  segIris(tempOriginal, outFilepath(outDir, filename, "_seg_iris", "pbm"), circles->iris);
 
-	Img *segIris = readPPM(outFilepath(outDir, filename, "_seg_iris", "pbm"));
-	Img *segIrisGrey = greyscale(segIris);
-	Img *segIrisGauss = gaussianFilter(segIrisGrey, 1);
-	Img *segThre = threshold(segIrisGauss, 0.33 * 255);
-	// Img *segThre = otsuMethod(segIrisGauss);
-	
-	int times = count(segThre, area);
-	printf("Comprometimento: %.2lf%\n\n", times/(double)area*100);
+  Img *segIris = readPPM(outFilepath(outDir, filename, "_seg_iris", "pbm"));
+  Img *segIrisGrey = greyscale(segIris);
+  Img *segIrisGauss = gaussianFilter(segIrisGrey, 1);
+  Img *segThre = threshold(segIrisGauss, 0.33 * 255);
+  // Img *segThre = otsuMethod(segIrisGauss);
 
-	writePPM(segThre, "test/teste.ppm");
+  int times = count(segThre);
+  printf("Comprometimento: %.2lf%\n\n", times / (double)area * 100);
 
-	// writePPM(tempOriginal, "test/original.ppm");
+  writePPM(segThre, "test/teste.ppm");
 
-	// Just a test to read/write pbm
-	// ImgBin *pbmImage = readPBM("test/Catarata_threshold.pbm");
-	// writePBM_PBM(pbmImage, "test/teste.pbm");
+  // writePPM(tempOriginal, "test/original.ppm");
 
-	// freeImgBin(pbmImage);
-	free(circles);
-	freeImg(thresholdImg);
-	freeImg(tempOriginal);
-	free(filepath);
-	free(filename);
-	free(format);
-	free(diagFile);
-	return 0;
+  // Just a test to read/write pbm
+  // ImgBin *pbmImage = readPBM("test/Catarata_threshold.pbm");
+  // writePBM_PBM(pbmImage, "test/teste.pbm");
+
+  // freeImgBin(pbmImage);
+  free(circles);
+  freeImg(thresholdImg);
+  freeImg(tempOriginal);
+  free(filepath);
+  free(filename);
+  free(format);
+  free(diagFile);
+  return 0;
 }
--- src/read.c
+++ src/read.c
@@ -4,87 +4,102 @@
 
 Img *readPPM(char *filepath)
 {
-	puts("\e[1mReading the input image...\e[0m\n");
-	FILE *image;
-	char buffer[16];
-	ushort height, width;
-	Img *img; // class of the original image
+  puts("\e[1mReading the input image...\e[0m\n");
+  FILE *image;
+  char buffer[16];
+  ushort height, width;
+  Img *img; // class of the original image
 
-	uchar max_rgb;
-	char *strippedFilepath = stripFilepath(filepath);
+  uchar max_rgb;
+  char *strippedFilepath = stripFilepath(filepath);
 
-	// opening the image for reading
-	image = fopen(filepath, "r");
-	if (!image) {
-		fprintf(stderr, "Can't open image '%s'.\n", strippedFilepath);
-		perror(filepath);
-		exit(1);
-	} else{
-		printf("Sucess! '%s' has been loaded.\n", strippedFilepath);
-	}
+  // opening the image for reading
+  image = fopen(filepath, "r");
+  if (!image)
+  {
+    fprintf(stderr, "Can't open image '%s'.\n", strippedFilepath);
+    perror(filepath);
+    exit(1);
+  }
+  else
+  {
+    printf("Sucess! '%s' has been loaded.\n", strippedFilepath);
+  }
 
-	// reading image format
-	if (!fgets(buffer, sizeof(buffer), image)) {
-		perror("Error reading image format.");
-		fclose(image);
-		return NULL; 
-	}
-	buffer[strlen(buffer)] = 0;
-	buffer[strlen(buffer) - 1] = '\0';
+  // reading image format
+  if (!fgets(buffer, sizeof(buffer), image))
+  {
+    perror("Error reading image format.");
+    fclose(image);
+    return NULL;
+  }
+  buffer[strlen(buffer)] = 0;
+  buffer[strlen(buffer) - 1] = '\0';
 
-	// checking image format
-	if (buffer[0] != 'P' || buffer[1] != '3') {
-		fprintf(stderr, "Wrong image format (should be 'P3'). Error reading '%s'.\n", strippedFilepath);
-		fclose(image);
-		return NULL; 
-	} else {
-		printf("The image format is '%s'.\n", buffer);
-	}
+  // checking image format
+  if (buffer[0] != 'P' || buffer[1] != '3')
+  {
+    fprintf(stderr, "Wrong image format (should be 'P3'). Error reading '%s'.\n", strippedFilepath);
+    fclose(image);
+    return NULL;
+  }
+  else
+  {
+    printf("The image format is '%s'.\n", buffer);
+  }
 
-	// looking for comments
-	char c;
-	c = getc(image);
-	while (c == '#') {
-		while (getc(image) != '\n') {
-			c = getc(image);
-			ungetc(c, image);
-		}
-	}
-	ungetc(c, image);
+  // looking for comments
+  char c;
+  c = getc(image);
+  while (c == '#')
+  {
+    while (getc(image) != '\n')
+    {
+      c = getc(image);
+      ungetc(c, image);
+    }
+  }
+  ungetc(c, image);
 
-	// reading height and width
-	if (!fscanf(image, "%hu %hu\n", &width, &height)) {
-		fprintf(stderr, "Invalid image size (error reading '%s').\n", strippedFilepath);
-		fclose(image);
-		return NULL;
-	} else {
-		printf("Dimensions identified (W x H): %ix%i pixels.\n", width, height);
-	}
-	
-	// check the max RGB size allowed fo the image
-	if (fscanf(image, "%hhu", &max_rgb) != 1) {
-		fprintf(stderr, "Invalid max RGB value.\n");
-	}
+  // reading height and width
+  if (!fscanf(image, "%hu %hu\n", &width, &height))
+  {
+    fprintf(stderr, "Invalid image size (error reading '%s').\n", strippedFilepath);
+    fclose(image);
+    return NULL;
+  }
+  else
+  {
+    printf("Dimensions identified (W x H): %ix%i pixels.\n", width, height);
+  }
 
-	// allocating memory for the image
-	img = createImg(height, width, max_rgb);
-	if (!img) {
-		fprintf(stderr, "Can't allocate memory (error reading '%s').\n", strippedFilepath);
-		freeImg(img);
-		fclose(image);
-		return NULL;
-	}
+  // check the max RGB size allowed fo the image
+  if (fscanf(image, "%hhu", &max_rgb) != 1)
+  {
+    fprintf(stderr, "Invalid max RGB value.\n");
+  }
 
-	// check if the pixel matrix is null
-	if (!img->pixels) {
-		fprintf(stderr, "Couldn't allocate the pixel matrix (error reading '%s').\n", strippedFilepath);
-		fclose(image);
-		freeImg(img);
-		return NULL;
-	}
+  // allocating memory for the image
+  img = createImg(height, width, max_rgb);
+  if (!img)
+  {
+    fprintf(stderr, "Can't allocate memory (error reading '%s').\n", strippedFilepath);
+    freeImg(img);
+    fclose(image);
+    return NULL;
+  }
 
-	// Starting the pixel mapping
-	/*
+  // check if the pixel matrix is null
+  if (!img->pixels)
+  {
+    fprintf(stderr, "Couldn't allocate the pixel matrix (error reading '%s').\n", strippedFilepath);
+    fclose(image);
+    freeImg(img);
+    return NULL;
+  }
+
+  // Starting the pixel mapping
+  /*
 		Little explanation:
 			For each 3 lines, we get a rgb(r, g, b), that gets us one pixel.
 			Each <Width> pixels, we get a horizontal line
@@ -92,111 +107,130 @@ Img *readPPM(char *filepath)
 
 			There shoud be $width times $height pixels.
 	*/
-	int loaded_pixels = 0;
-	for (int i = 0; i < img->height; ++i) {
-		for (int j = 0; j < img->width; ++j) {
-			fscanf(image, "%hhu\n%hhu\n%hhu\n", &img->pixels[i][j].r, &img->pixels[i][j].g, &img->pixels[i][j].b);
-			++loaded_pixels;
-			// if you want to see it for yourself in action, uncomment the next line
-			// printf("rgb(%i,%i,%i)\n", img->pixels[i][j].r, img->pixels[i][j].g, img->pixels[i][j].b);
-		}
-	}
+  int loaded_pixels = 0;
+  for (int i = 0; i < img->height; ++i)
+  {
+    for (int j = 0; j < img->width; ++j)
+    {
+      fscanf(image, "%hhu\n%hhu\n%hhu\n", &img->pixels[i][j].r, &img->pixels[i][j].g, &img->pixels[i][j].b);
+      ++loaded_pixels;
+      // if you want to see it for yourself in action, uncomment the next line
+      // printf("rgb(%i,%i,%i)\n", img->pixels[i][j].r, img->pixels[i][j].g, img->pixels[i][j].b);
+    }
+  }
 
-	// check if the number of pixels loaded equals to the number of real pixels.
-	if (!(loaded_pixels == img->height * img->width)) {
-		fprintf(stderr, "Couldn't load correcly the pixel matrix (should be '%i' pixels, instead, got '%i').\n", img->height * img->width, loaded_pixels);
-		fclose(image);
-		freeImg(img);
-		return NULL;
-	} else {
-		printf("Correctly loaded %i pixels.\n", loaded_pixels);
-	}
-	
-	puts("\n\e[1mFinished reading image...\e[0m");
+  // check if the number of pixels loaded equals to the number of real pixels.
+  if (!(loaded_pixels == img->height * img->width))
+  {
+    fprintf(stderr, "Couldn't load correcly the pixel matrix (should be '%i' pixels, instead, got '%i').\n", img->height * img->width, loaded_pixels);
+    fclose(image);
+    freeImg(img);
+    return NULL;
+  }
+  else
+  {
+    printf("Correctly loaded %i pixels.\n", loaded_pixels);
+  }
 
-	fclose(image);
-	free(strippedFilepath);
-	return img;
+  puts("\n\e[1mFinished reading image...\e[0m");
+
+  fclose(image);
+  free(strippedFilepath);
+  return img;
 }
 
 ImgBin *readPBM(char *filepath)
 {
-	puts("\e[1mReading the binary image...\e[0m\n");
-	FILE *image;
-	char buffer[16];
-	ushort height, width;
-	ImgBin *img; // class of the original image
+  puts("\e[1mReading the binary image...\e[0m\n");
+  FILE *image;
+  char buffer[16];
+  ushort height, width;
+  ImgBin *img; // class of the original image
 
-	// uchar max_rgb; // not needed, because pbm does not have it
-	char *strippedFilepath = stripFilepath(filepath);
+  // uchar max_rgb; // not needed, because pbm does not have it
+  char *strippedFilepath = stripFilepath(filepath);
 
-	// opening the image for reading
-	image = fopen(filepath, "r");
-	if (!image) {
-		fprintf(stderr, "Can't open image '%s'.\n", strippedFilepath);
-		perror(filepath);
-		exit(1);
-	} else{
-		printf("Sucess! '%s' has been loaded.\n", strippedFilepath);
-	}
+  // opening the image for reading
+  image = fopen(filepath, "r");
+  if (!image)
+  {
+    fprintf(stderr, "Can't open image '%s'.\n", strippedFilepath);
+    perror(filepath);
+    exit(1);
+  }
+  else
+  {
+    printf("Sucess! '%s' has been loaded.\n", strippedFilepath);
+  }
 
-	// reading image format
-	if (!fgets(buffer, sizeof(buffer), image)) {
-		perror("Error reading image format.");
-		fclose(image);
-		return NULL; 
-	}
-	buffer[strlen(buffer)] = 0;
-	buffer[strlen(buffer) - 1] = '\0';
+  // reading image format
+  if (!fgets(buffer, sizeof(buffer), image))
+  {
+    perror("Error reading image format.");
+    fclose(image);
+    return NULL;
+  }
+  buffer[strlen(buffer)] = 0;
+  buffer[strlen(buffer) - 1] = '\0';
 
-	// checking image format
-	if (buffer[0] != 'P' || buffer[1] != '1') {
-		fprintf(stderr, "Wrong image format (should be 'P1'). Error reading '%s'.\n", strippedFilepath);
-		fclose(image);
-		return NULL; 
-	} else {
-		printf("The image format is '%s'.\n", buffer);
-	}
+  // checking image format
+  if (buffer[0] != 'P' || buffer[1] != '1')
+  {
+    fprintf(stderr, "Wrong image format (should be 'P1'). Error reading '%s'.\n", strippedFilepath);
+    fclose(image);
+    return NULL;
+  }
+  else
+  {
+    printf("The image format is '%s'.\n", buffer);
+  }
 
-	// looking for comments
-	char c;
-	c = getc(image);
-	while (c == '#') {
-		while (getc(image) != '\n') {
-			c = getc(image);
-			ungetc(c, image);
-		}
-	}
-	ungetc(c, image);
+  // looking for comments
+  char c;
+  c = getc(image);
+  while (c == '#')
+  {
+    while (getc(image) != '\n')
+    {
+      c = getc(image);
+      ungetc(c, image);
+    }
+  }
+  ungetc(c, image);
 
-	// reading height and width
-	if (!fscanf(image, "%hu %hu\n", &width, &height)) {
-		fprintf(stderr, "Invalid image size (error reading '%s').\n", strippedFilepath);
-		fclose(image);
-		return NULL;
-	} else {
-		printf("Dimensions identified (W x H): %ix%i pixels.\n", width, height);
-	}
-	
-	// allocating memory for the image
-	img = createImgBin(height, width);
-	if (!img) {
-		fprintf(stderr, "Can't allocate memory (error reading '%s').\n", strippedFilepath);
-		freeImgBin(img);
-		fclose(image);
-		return NULL;
-	}
+  // reading height and width
+  if (!fscanf(image, "%hu %hu\n", &width, &height))
+  {
+    fprintf(stderr, "Invalid image size (error reading '%s').\n", strippedFilepath);
+    fclose(image);
+    return NULL;
+  }
+  else
+  {
+    printf("Dimensions identified (W x H): %ix%i pixels.\n", width, height);
+  }
 
-	// check if the pixel matrix is null
-	if (!img->pixels) {
-		fprintf(stderr, "Couldn't allocate the pixel matrix (error reading '%s').\n", strippedFilepath);
-		fclose(image);
-		freeImgBin(img);
-		return NULL;
-	}
+  // allocating memory for the image
+  img = createImgBin(height, width);
+  if (!img)
+  {
+    fprintf(stderr, "Can't allocate memory (error reading '%s').\n", strippedFilepath);
+    freeImgBin(img);
+    fclose(image);
+    return NULL;
+  }
 
-	// Starting the pixel mapping
-	/*
+  // check if the pixel matrix is null
+  if (!img->pixels)
+  {
+    fprintf(stderr, "Couldn't allocate the pixel matrix (error reading '%s').\n", strippedFilepath);
+    fclose(image);
+    freeImgBin(img);
+    return NULL;
+  }
+
+  // Starting the pixel mapping
+  /*
 		Little explanation:
 			Each line will give us that row, each pixel will be a number followed with \s
 			Each <Width> pixels, we get a horizontal line
@@ -204,33 +238,39 @@ ImgBin *readPBM(char *filepath)
 
 			There shoud be $width times $height pixels.
 	*/
-	int loaded_pixels = 0;
-	int temp_pixel;
-	for (int i = 0; i < img->height; ++i) {
-		for (int j = 0; j < img->width; ++j) {
-			fscanf(image, "%i", &temp_pixel);
-			if (temp_pixel >= 0 && temp_pixel <= 1){
-				img->pixels[i][j] = temp_pixel;
-			}
-			++loaded_pixels;
-			// if you want to see it for yourself in action, uncomment the next line
-			// printf("pixel(%i)\n", img->pixels[i][j]);
-		}
-	}
+  int loaded_pixels = 0;
+  int temp_pixel;
+  for (int i = 0; i < img->height; ++i)
+  {
+    for (int j = 0; j < img->width; ++j)
+    {
+      fscanf(image, "%i", &temp_pixel);
+      if (temp_pixel >= 0 && temp_pixel <= 1)
+      {
+        img->pixels[i][j] = temp_pixel;
+      }
+      ++loaded_pixels;
+      // if you want to see it for yourself in action, uncomment the next line
+      // printf("pixel(%i)\n", img->pixels[i][j]);
+    }
+  }
 
-	// check if the number of pixels loaded equals to the number of real pixels.
-	if (!(loaded_pixels == img->height * img->width)) {
-		fprintf(stderr, "Couldn't load correcly the pixel matrix (should be '%i' pixels, instead, got '%i').\n", img->height * img->width, loaded_pixels);
-		fclose(image);
-		freeImgBin(img);
-		return NULL;
-	} else {
-		printf("Correctly loaded %i pixels.\n", loaded_pixels);
-	}
-	
-	printf("\n\e[1mFinished reading the binary image...\e[0m\n");
+  // check if the number of pixels loaded equals to the number of real pixels.
+  if (!(loaded_pixels == img->height * img->width))
+  {
+    fprintf(stderr, "Couldn't load correcly the pixel matrix (should be '%i' pixels, instead, got '%i').\n", img->height * img->width, loaded_pixels);
+    fclose(image);
+    freeImgBin(img);
+    return NULL;
+  }
+  else
+  {
+    printf("Correctly loaded %i pixels.\n", loaded_pixels);
+  }
 
-	fclose(image);
-	free(strippedFilepath);
-	return img;
+  printf("\n\e[1mFinished reading the binary image...\e[0m\n");
+
+  fclose(image);
+  free(strippedFilepath);
+  return img;
 }
